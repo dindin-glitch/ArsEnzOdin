7ccb185ce4775737d2ca9e69ec1856d5f80d2e58
dindin-glitch <66629792+dindin-glitch@users.noreply.github.com>
2020-06-11 02:00:35 +0200

    Bonne journée de prog!

---
+++ Code/ArsEnzOdin/ArsEnzOdin/ArsEnzOdin.Android/Account.cs
@@ -0,0 +1,58 @@
+﻿using Xamarin.Forms.Platform.Android;
+
+namespace System.IO
+{
+
+    namespace ArsEnzOdin
+    {
+
+        public static class Account
+        {
+            public static int AccountWork(string name, int age)
+            {
+                string line = "Name = " + name;
+                int line_number = 1;
+                int line_to_edit = 1;
+
+                using (StreamReader reader = new StreamReader(@"C:\Users\odinb\Githubs\ArsEnzOdin\Code\ArsEnzOdin\ArsEnzOdin\ArsEnzOdin.Android\Resources\AccountData.txt"))
+                {
+                    using (StreamWriter writer = new StreamWriter(@"C:\Users\odinb\Githubs\ArsEnzOdin\Code\ArsEnzOdin\ArsEnzOdin\ArsEnzOdin.Android\Resources\AccountData.txt"))
+                    {
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            if (line_number == line_to_edit)
+                            {
+                                writer.WriteLine(line);
+                            }
+                            else
+                            {
+                                writer.WriteLine("");
+                            }
+
+                            line_number++;
+                        }
+                    }
+                }
+                line = "Age = " + age;
+                line_to_edit = 2;
+                line_number = 1;
+                using (StreamReader reader = new StreamReader(@"C:\Users\odinb\Githubs\ArsEnzOdin\Code\ArsEnzOdin\ArsEnzOdin\ArsEnzOdin.Android\Resources\AccountData.txt"))
+                {
+                    using (StreamWriter writer = new StreamWriter(@"C:\Users\odinb\Githubs\ArsEnzOdin\Code\ArsEnzOdin\ArsEnzOdin\ArsEnzOdin.Android\Resources\AccountData.txt"))
+                    {
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            if (line_number == line_to_edit)
+                            {
+                                writer.WriteLine(line);
+                            }
+
+                            line_number++;
+                        }
+                    }
+                }
+                return 0;
+            }
+        }
+    }
+}
--- Code/ArsEnzOdin/ArsEnzOdin/ArsEnzOdin.Android/MainActivity.cs
+++ Code/ArsEnzOdin/ArsEnzOdin/ArsEnzOdin.Android/MainActivity.cs
@@ -6,12 +6,18 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using static System.IO.ArsEnzOdin.Account;
 
 namespace ArsEnzOdin.Droid
 {
     [Activity(Label = "ArsEnzOdin", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        const string name = ""; //name recuperred idk how
+        const int age = 0; //same, but for the age
+
+        private int AccountWork(string name, int age); //why it doesn't work?? please correct this idk why.
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
