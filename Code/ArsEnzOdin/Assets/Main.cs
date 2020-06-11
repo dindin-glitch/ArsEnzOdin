4f05a24426d8e4a2c24994fdf1a39deb5c8d4ce0
dindin-glitch <66629792+dindin-glitch@users.noreply.github.com>
2020-06-11 19:51:55 +0200

    lol

--- Code/ArsEnzOdin/Assets/Main.cs
+++ Code/ArsEnzOdin/Assets/Main.cs
@@ -1,16 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Main : MonoBehaviour {
+    
+    public int level = 0;
+    public Text levelTexte;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start () {
         
     }
+
+    // Update is called once per frame
+    void Update () {
+        levelTexte.text = ("Level: " + level);
+    }
+    public void LevelAdd () {
+        level += 1;
+        
+        
+    }
+    
 }
